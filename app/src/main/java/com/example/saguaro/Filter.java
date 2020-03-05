b1874ea1630817cb8d8d2a42f7b1d0e7d9044c2a
Hearner <quentin_ch@hotmail.fr>
2020-03-05 12:03:27 +0100

    Filter on swipe

--- app/src/main/java/com/example/saguaro/Filter.java
+++ app/src/main/java/com/example/saguaro/Filter.java
@@ -2,7 +2,10 @@ package com.example.saguaro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
@@ -10,6 +13,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.Map;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -24,11 +28,48 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.mukesh.image_processing.ImageProcessor;
+
 public class Filter extends AppCompatActivity {
     private Button btn;
     private ImageView imageview;
     private static final String IMAGE_DIRECTORY = "/SaguanoPicture";
     private int GALLERY = 1, CAMERA = 2;
+    private Bitmap bitmap;
+
+    private float x1, x2;
+    static final int MIN_DISTANCE = 150;
+
+    public static enum FilterList {
+        Brightness,
+        Contrast,
+        CrossProcess,
+        Documentary,
+        DuoTone,
+        Fill_Light,
+        FishEye,
+        Flip_Horizontally,
+        Flip_Vertically,
+        Grain,
+        Grayscale,
+        Highlight,
+        Lomoish,
+        Negative,
+        None,
+        Posterize,
+        Rotate,
+        Saturate,
+        Sepia,
+        Sharpen,
+        Temperature,
+        Tint,
+        Vignette;
+        private static FilterList[] vals = values();
+
+        public FilterList next() {
+            return vals[(this.ordinal() + 1) % vals.length];
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,8 +147,8 @@ public class Filter extends AppCompatActivity {
             if (data != null) {
                 Uri contentURI = data.getData();
                 try {
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
-                    String path = saveImage(bitmap);
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                 //   String path = saveImage(bitmap);
                     Toast.makeText(Filter.this, "Image Saved!", Toast.LENGTH_SHORT).show();
                     imageview.setImageBitmap(bitmap);
 
@@ -120,7 +161,7 @@ public class Filter extends AppCompatActivity {
         } else if (requestCode == CAMERA) {
             Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
             imageview.setImageBitmap(thumbnail);
-            saveImage(thumbnail);
+            //saveImage(thumbnail);
             Toast.makeText(Filter.this, "Image Saved!", Toast.LENGTH_SHORT).show();
         }
     }
@@ -152,5 +193,39 @@ public class Filter extends AppCompatActivity {
             e1.printStackTrace();
         }
         return "";
+    }
+
+    /**
+     * Detect swipe left to right or right to left
+     *
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+                if (deltaX > MIN_DISTANCE) {
+                    Toast.makeText(this, "left2right swipe", Toast.LENGTH_SHORT).show();
+
+                   // bitmap = BitmapFactory.decodeResource(getResources(), R.id.imageview);
+                    ImageProcessor imageProcessor =  new ImageProcessor();
+                    bitmap  = imageProcessor.doInvert(bitmap);
+                    imageview = (ImageView) findViewById(R.id.imageview);
+                    imageview.setImageBitmap(bitmap);
+
+                } else if (deltaX < MIN_DISTANCE) {
+                    Toast.makeText(this, "right2left swipe", Toast.LENGTH_SHORT).show();
+                } else {
+                    // consider as something else - a screen tap for example
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 }
