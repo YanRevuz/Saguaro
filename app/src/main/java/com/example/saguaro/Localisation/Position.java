8dfc22da844506c8a68f05c1383adf35158bcf21
AlexisMat <mateo.alexis82@gmail.com>
2020-03-05 10:45:53 +0100

    Refactor: création de la méthode getCritere() pour recupérer un objet Criteria utilisé pour la recherche de fournisseur

--- app/src/main/java/com/example/saguaro/Localisation/Position.java
+++ app/src/main/java/com/example/saguaro/Localisation/Position.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
@@ -15,39 +14,47 @@ import androidx.core.app.ActivityCompat;
 public class Position {
 
     private String fournisseur;
+
     private LocationManager locationManager;
+
     private Activity activity;
 
     public Position(Activity act) {
         this.activity = act;
     }
 
-    public Location getProvider() {
-        locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+    private Criteria getCritere() {
         Criteria critere = new Criteria();
-
 // Pour indiquer la précision voulue
 // On peut mettre ACCURACY_FINE pour une haute précision ou ACCURACY_COARSE pour une moins bonne précision
         critere.setAccuracy(Criteria.ACCURACY_COARSE);
-
 // Est-ce que le fournisseur doit être capable de donner une altitude ?
         critere.setAltitudeRequired(false);
-
 // Est-ce que le fournisseur doit être capable de donner une direction ?
         critere.setBearingRequired(false);
-
 // Est-ce que le fournisseur peut être payant ?
         critere.setCostAllowed(false);
-
 // Pour indiquer la consommation d'énergie demandée
 // Criteria.POWER_HIGH pour une haute consommation, Criteria.POWER_MEDIUM pour une consommation moyenne et Criteria.POWER_LOW pour une basse consommation
         critere.setPowerRequirement(Criteria.POWER_MEDIUM);
-
 // Est-ce que le fournisseur doit être capable de donner une vitesse ?
         critere.setSpeedRequired(false);
+        return critere;
+    }
 
+//    private void requestLocationPermission() {
+//        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+//            new AlertDialog.Builder(activity).setMessage("R string request permission").setPositiveButton(android.R.string.ok, (dialog, wich) -> {
+//                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, );
+//            }).setNegativeButton(android.R.string.cancel, (dialog,wich) -> {
+//                activity.finish(); }).create();
+//        }
+//    }
+
+    public Location getProvider() {
+        locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        Criteria critere = this.getCritere();
         fournisseur = locationManager.getBestProvider(critere, true);
-
         if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -66,5 +73,27 @@ public class Position {
         return locationManager.getLastKnownLocation(fournisseur);
     }
 
+    public String getFournisseur() {
+        return fournisseur;
+    }
 
+    public void setFournisseur(String fournisseur) {
+        this.fournisseur = fournisseur;
+    }
+
+    public LocationManager getLocationManager() {
+        return locationManager;
+    }
+
+    public void setLocationManager(LocationManager locationManager) {
+        this.locationManager = locationManager;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
 }
